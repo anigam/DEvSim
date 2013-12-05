e7ece61e13c5a59323a023d05c82537f5d24ac5b
anigam <anigam@nd.edu>
2013-12-04 21:48:07 -0500

    Update Scheduler.java

--- DEvSim/src/system/Scheduler.java
+++ DEvSim/src/system/Scheduler.java
@@ -9,8 +9,8 @@ import process.Process;
 public class Scheduler {
 
 	private Vector<Process> ProcessList;
-	
-	public void addProcess(Vector<Process> ProcessList)
+
+	public Scheduler(Vector<Process> ProcessList)
 	{
 		this.ProcessList=ProcessList;
 	}
@@ -19,20 +19,38 @@ public class Scheduler {
 	{
 		Event e=null;
 		Process curProcess=ProcessList.elementAt(0);
+		System.out.println("The current procees is P:"+ curProcess.getpid());
+		System.out.println("Number of events in process:" + curProcess.get_remaining_event_num());
 		e=curProcess.popEvent();
 		return e;
+	}
+	
+	public void addEventback(Event e)
+	{
+		Process curProcess=ProcessList.elementAt(0);
+		System.out.println("The current procees is P:"+ curProcess.getpid());
+		curProcess.pushFirst(e);
+	}
+	
+	public int getNumberactiveprocess()
+	{
+		return ProcessList.size();
 	}
 	
 	public boolean contextSwitch()
 	{
 		 Process curProcess = ProcessList.remove(0);
+		 System.out.println("before context The current procees is P:"+ curProcess.getpid());
+		 System.out.println("For the process witching out the number of remaining events are: "+ curProcess.get_remaining_event_num());
+		 
 		 if (curProcess.get_remaining_event_num()>0)
 		 {
 			 ProcessList.add(curProcess);
 			 return true;
 		 }
-		 if(ProcessList.isEmpty())
-			 return false;
+		 if(!ProcessList.isEmpty())
+			 return true;
+		 
 		 return false;
 	}
 }
