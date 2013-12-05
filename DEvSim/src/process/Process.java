a3c30c1fbf39e229080089367db91c6b8dc721fd
anigam <anigam@nd.edu>
2013-12-04 21:55:39 -0500

    Added a condition in popEvent() ---Aastha

--- DEvSim/src/process/Process.java
+++ DEvSim/src/process/Process.java
@@ -19,8 +19,15 @@ public class Process {
 		return this.EventList.size();
 	}
 
-	public Event popEvent() {
+	/*public Event popEvent() {
 		return this.EventList.remove(0);
+	}*/
+	
+	public Event popEvent() {
+		if(EventList.size()>0)
+			return this.EventList.remove(0);
+		else 
+			return null;
 	}
 
 	//at the end of one quantum, if one event E still does not finish, add it into the first place of the EventList, so that next time when this Process obtain the right to use CPU, it can continue to execute E.
