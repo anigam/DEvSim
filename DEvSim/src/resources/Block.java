3423dc5acf6be302c2a3db354acc3ec6482ff0f5
anigam <anigam@nd.edu>
2013-12-04 21:57:10 -0500

    Added the Block constructor--Aastha

--- DEvSim/src/resources/Block.java
+++ DEvSim/src/resources/Block.java
@@ -5,5 +5,12 @@ public class Block {
 	int diskPos;
 	int lastAccess;
 	boolean dirty;
+	
+	public Block(int diskPos, int lastAcces, boolean dirty)
+	{
+		this.diskPos=diskPos;
+		this.lastAccess=lastAcces;
+		this.dirty=dirty;
+	}
 
 }
